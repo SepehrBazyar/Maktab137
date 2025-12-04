ae113d7082ac6631601432e5671cfafb5ee62ec4
Sepehr Bazyar <sepehr.bazyar@karnameh.ir>
2025-12-04 17:00:20 +0330

    feat: users

--- session_09.py
+++ session_09.py
@@ -1,20 +1,18 @@
-from flask import Flask
+from flask import Flask, Response, request
 
 
 app = Flask(__name__)
 
-users = [
-    {
-        "id": 1,
+users = {
+    1: {
         "name": "akbar",
         "phone_number": "09123456789",
     },
-    {
-        "id": 2,
+    2: {
         "name": "asqar",
         "phone_number": "09129876543",
     },
-]
+}
 
 @app.route("/hello")
 def hello_world():
@@ -22,5 +20,27 @@ def hello_world():
         "message": "Hello World!",
     }
 
+
+@app.route("/profile/<int:id>")
+def profile(id: int):
+    user = users.get(id)
+    if user is not None:
+        return user
+
+    return Response(
+        "NOT FOUND",
+        status=404,
+    )
+
+
+@app.route("/profile/<id>")
+def profile_test(id):
+    return f"Hello {id}"
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    users[3] = request.json
+    return "OK"
 
 app.run()
